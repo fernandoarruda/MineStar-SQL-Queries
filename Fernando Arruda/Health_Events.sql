7d23bfb7b30d4b3271451fcd230f3e179cc0528e
Fernando Arruda <fernandoarruda1384@gmail.com>
2025-04-09 00:32:43 -0300

    Consulta HealthEvents - Adicionado tempo em hh:mm:ss, latitude..., e data sample

--- Fernando Arruda/Health_Events.sql
+++ Fernando Arruda/Health_Events.sql
@@ -1,33 +1,40 @@
+  SELECT TOP (100)
+	   [timestamp] AS 'Data da Ocorrência',
+       [type] AS 'Tipo',
+	   [machine_N] AS 'Máquina',
+	   [machineClass_N] AS 'Frota',
+	   [eventNumber] AS 'Evento',
+	   [eventLevel] AS 'Nível do evento',
+       [alarmDescription] AS 'Descrição do alarme',
+	   [mid] AS 'MID',
+       [mid_N] AS 'MID Descrição',
+       [cid] AS 'CID',
+       [cid_N] AS 'CID Descrição',
+       [fmi] AS 'FMI',
+       [fmi_N] AS 'FMI Descrição',
+       [worstUnitType] AS 'Tipo do Pior Valor Registrado',
+       [worstValue] AS 'Pior Valor Registrado',
+       [worstValue_D] AS 'Decodificação do Pior Valor Registrado',
+       [worstValue_U] AS 'Unidade do Pior Valor Registrado',
+       [payload_Q] AS 'Payload',
+       [duration] AS 'Duração em segundos',
 
-SELECT --TOP (100)
-	   [timestamp] as 'Data da Ocorrência'
-      ,[type] as 'Tipo'
-	  ,[machine_N] as 'Máquina'
-	  ,[machineClass_N] as 'Frota'
-	  ,[eventNumber] as 'Evento'
-	  ,[eventLevel] as 'Nível do evento'
-      ,[alarmDescription] AS 'Descrição do alarme'
-	  ,[mid] as 'MID'
-      ,[mid_N] as 'MID Descrição'
-      ,[cid] as 'CID'
-      ,[cid_N] as 'CID Descrição'
-      ,[fmi] as 'FMI'
-      ,[fmi_N] as 'FMI Descrição'
-      ,[worstUnitType] as 'Tipo do Pior Valor Registrado'
-      ,[worstValue] as 'Pior Valor Registrado'
-      ,[worstValue_D] as 'Decodificação do Pior Valor Registrado'
-      ,[worstValue_U] as 'Unidade do Pior Valor Registrado'
-      ,[payload_Q] as 'Payload'
-      ,[duration] as 'Duração em segundos'
-      ,ROUND([smu_Q], 2) as 'Horímetro'
-	  ,[operator_N] as 'Operador'
-      ,[speed_Q] as 'Velocidade'
-      ,[XYZX] as 'X'
-      ,[XYZY] as 'Y'
-      ,[XYZZ] as 'Z'
+       -- Conversão para hh:mm:ss
+       RIGHT('0' + CAST(CAST([duration] AS INT) / 3600 AS VARCHAR(2)), 2) + ':' +
+       RIGHT('0' + CAST((CAST([duration] AS INT) % 3600) / 60 AS VARCHAR(2)), 2) + ':' +
+       RIGHT('0' + CAST(CAST([duration] AS INT) % 60 AS VARCHAR(2)), 2) AS 'Duração (hh:mm:ss)',
+
+       ROUND([smu_Q], 2) AS 'Horímetro',
+	   [operator_N] AS 'Operador',
+       [speed_Q] AS 'Velocidade',
+       [XYZX] AS 'Latitude(X)',
+       [XYZY] AS 'Longitude (Y)',
+       [XYZZ] AS 'Altura (Z)'
+
 FROM [mshist].[dbo].[V_HEALTH_EVENT]
-WHERE locationTimestamp > '2025-01-31 23:59:59.000' --AND  locationTimestamp < '2025-03-01 00:00:01.000'
+WHERE locationTimestamp > '2025-01-31 23:59:59.000' 
   AND duration IS NOT NULL
+  --AND  locationTimestamp < '2025-03-01 00:00:01.000'
   --AND type = 'Health Maintenance' -- Eventos de sistema apenas
   --AND type = 'Health Dat'a --Eventos máquina
   --AND alarmDescription like '%%' --filtrar por evento especifico
