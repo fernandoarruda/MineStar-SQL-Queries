828e77ac420cf01264bca9b88f1edb91e5bed630
fernandoarruda <fernandoarruda1384@gmail.com>
2025-04-27 17:09:39 -0300

    Update DIG_report.sql
    Columns reordered, local time conversion added and core coordinates removed/commented.

--- Fernando Arruda/DIG_report.sql
+++ Fernando Arruda/DIG_report.sql
@@ -2,28 +2,30 @@ SELECT
     -- From Bucket Load
     b.BUCKET_LOAD_ID,
     b.MACHINE_ID,
-    m.NAME AS LOADER_NAME, -- NEW: Loader machine name
-    b.TIME_END,
+    m.NAME AS LOADER_NAME,
+	b.TIME_END,
+    DATEADD(HOUR, -3, b.TIME_END) AS TIME_END_LOCAL,
     b.MATERIAL,
+    t.OVERRIDE_MATERIAL, -- << Moved here, just after MATERIAL
     
-    -- Coordinates for COR
-    cor.X AS COR_X,
-    cor.Y AS COR_Y,
-    cor.Z AS COR_Z,
+    /*Coordinates for COR - Ccoordinates for centre of
+rotation*/
+    --cor.X AS COR_X,
+    --cor.Y AS COR_Y,
+    --cor.Z AS COR_Z,
     
     -- Coordinates for DIG
     dig.X AS DIG_X,
     dig.Y AS DIG_Y,
     dig.Z AS DIG_Z,
     
-    -- Coordinates for DUMP
+    -- Coordinates for DUMP of the loader bucket
     dump.X AS DUMP_X,
     dump.Y AS DUMP_Y,
     dump.Z AS DUMP_Z,
     
     -- From Truck Load
     t.TRUCK_ID,
-    t.OVERRIDE_MATERIAL,
     
     -- Truck name from CA_C_TRUCK
     ct.NAME AS TRUCK_NAME
@@ -54,8 +56,8 @@ LEFT JOIN [TerrainProdFiles].[dbo].[CA_P_TRUCK_LOAD] AS t
 LEFT JOIN [TerrainProdFiles].[dbo].[CA_C_TRUCK] AS ct
     ON t.TRUCK_ID = ct.TRUCK_ID
 
--- NEW: Join Machine table to get Loader Name
+-- Join Machine table to get Loader Name
 LEFT JOIN [TerrainProdFiles].[dbo].[CA_C_MACHINE] AS m
     ON b.MACHINE_ID = m.MACHINE_ID
 
-WHERE b.TIME_END >= '2024-02-01'; -- Optional filter
+--WHERE b.TIME_END >= '2025-02-01'; -- Optional filter
